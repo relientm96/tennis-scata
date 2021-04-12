40b7306bd0ad86b60fe98b7c5f04e8799bfddd68
Matthew Yong <myong@seek.com.au>
2021-04-12 23:00:38 +1000

    Adding comments to main driver

--- tennis-scata/src/main/scala/Main.scala
+++ tennis-scata/src/main/scala/Main.scala
@@ -1,8 +1,22 @@
 object Main extends App {
 
-  val gameResults = List(1, 1, 1, 1)
+  /**
+  * Goal that I had for this tennis kata
+  * Input is a List[Int] = (1, 0, 1 ...)
+  *   -> where 1, 0 represents whether player 2 or 1 wins that point
+  * Output:
+      -> List[List[Int]] where it represents score of player 1 and 2
+      -> Input: (0, 0, 0, 0) 
+      -> Output: (40, 0)
 
-  //val scores: List[List[Int]] = Tennis.playAGame(gameResults)
+      -> Input: (1, 1, 1, 1)
+      -> Output: (0, 40)
 
-  println(gameResults)
+      -> Input: (1, 0, 0, 1, 1)
+      -> Output: (30, 40)
+  */
+
+  //val gameResults: List[Int] = List(1, 1, 1, 1) // Returns [0, 40]
+  // gameResults2: List[Int] = List(0, 0, 0, 0) // Returns [40. 0]
+
 }
